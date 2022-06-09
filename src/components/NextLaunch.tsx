c78ac8357c8f24ed9cd96bb903862743f1766bad
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2022-06-09 16:20:50 -0300

    first commit

---
+++ src/components/NextLaunch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface INextLaunchProps {
+
+}
+
+function NextLaunch(props: INextLaunchProps) {
+
+  return (
+    <div>NextLaunch:- </div>
+  )
+}
+
+export default NextLaunch;
